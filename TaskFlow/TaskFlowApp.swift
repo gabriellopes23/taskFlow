3e4563000cac64c2b0bc58baaebca8252ad164f8
Gabriel lopes <lopessantosgabriel@outlook.com>
2024-11-01 14:04:03 -0300

    Initial Commit

---
+++ TaskFlow/TaskFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskFlowApp.swift
+//  TaskFlow
+//
+//  Created by Gabriel Lopes on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
